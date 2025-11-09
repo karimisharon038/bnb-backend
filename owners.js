bf27727a1f601089e3d2fe3fc88cdd27ad31a39a
karimisharon038 <karimisharon038@gmail.com>
2025-11-09 15:20:06 +0300

    Add files via upload

---
+++ owners.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const ownersSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  whatsapp: { type: String, required: true },
+  phone: { type: String, required: true },
+});
+
+// ✅ Prevent OverwriteModelError
+module.exports = mongoose.models.Owners || mongoose.model("Owners", ownersSchema);
+
