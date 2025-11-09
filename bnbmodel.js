d0d93304430b218f7931a970d9adbe5210e6a807
karimisharon038 <karimisharon038@gmail.com>
2025-11-09 17:35:43 +0300

    Add files via upload

---
+++ bnbmodel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const bnbSchema = new mongoose.Schema({
+ owner: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: "Owners",  // ✅ Must match owners.js
+  required: true,
+},
+  name: { type: String, required: true },
+  location: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  available: { type: Boolean, default: true },
+  images:{type: [String], default: []}
+}, { timestamps: true });
+
+module.exports = mongoose.model("BnB", bnbSchema);
